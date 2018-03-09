9c795b15ee32461ae810293298caf5daa9d3c0bd
wangxue <wangxue@coocaa.com>
2018-03-09 16:53:21 +0800

    增减日志 log4j2

--- src/main/java/com/wx/controller/TestController.java
+++ src/main/java/com/wx/controller/TestController.java
@@ -2,17 +2,19 @@ package com.wx.controller;
 
 import com.wx.entity.User;
 import com.wx.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
 @RequestMapping(value = "/testController")
 public class TestController {
 
     private final UserService userService;
+    private Logger logger = LogManager.getLogger("myLog");
 
     @Autowired
     public TestController(UserService userService) {
@@ -20,9 +22,12 @@ public class TestController {
     }
 
     @RequestMapping(value = "/test")
+    @ResponseBody
     public User test(Integer userId){
         System.out.println("test...");
-        return userService.queryById(userId);
+        User user = userService.queryById(userId);
+        logger.info("test...");
+        return user;
     }
 
 }
